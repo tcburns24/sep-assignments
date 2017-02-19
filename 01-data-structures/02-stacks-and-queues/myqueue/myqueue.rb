80b2d3dd79bcf7b47a98cb5d0245ce860d6ea1f5
Thomas Burns <tcburns24@gmail.com>
2017-02-19 11:44:21 -0800

    all MyQueue tests pass

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,25 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+		@tail = @queue[-1] # added by Tom
   end
 
   def enqueue(element)
+		@queue.insert(0, element)
+		@head = @queue[0]
+		@tail = @queue[-1]
   end
 
   def dequeue
+		@queue.delete(@queue[-1])
+		@tail = @queue[-1]
   end
 
   def empty?
+		if @queue.length == 0
+			return true
+		else
+			return false
+		end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -8,13 +8,13 @@ class MyStack
 		@top_index += 1
 		@stack[@top_index] = item
   end
-
+	
 	# item0 | item1 | item2 | item3
 	# @top_index = 3
 	# @top_index -= 1 # 2
 	# @top_index -= 1 # 1
 	# @top_index -= 1 # 0
-	# @top_index -= 1 #  -1
+	# @top_index -= 1 # -1
 	
 	def top
 		if @top_index > -1
